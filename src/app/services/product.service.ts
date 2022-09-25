d2155f7a5440b49d6c04957f503c9d3baf1b2f03
AzerH <hasanov.azar@gmail.com>
2022-09-25 16:59:33 +0400

    25.09

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,6 +6,7 @@ import { ListResponseModel } from '../models/listResponseModel';
 
 
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 
 @Injectable({
@@ -26,5 +27,10 @@ export class ProductService {
     let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
       
+  }
+  add(product:Product):Observable<ResponseModel>
+  {
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product)
+
   }
 }
